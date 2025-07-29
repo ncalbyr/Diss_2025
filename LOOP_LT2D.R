75f02b4ad4bb6ebe03a6ea9b60f09bae1ec2538d
Noah Byrne <noahbyrne.153@gmail.com>
2025-07-29 14:34:35 +0100

    unecessary text

--- LOOP_LT2D.R
+++ LOOP_LT2D.R
@@ -69,3 +69,7 @@ abline(v = 210, col = "red", lwd = 2)
 abline(v = mean_abund_lt2d , col = "blue", lwd = 2, lty = 2)
 legend("topright", legend = c("True Abundance", "Mean Estimate"),
        col = c("red", "blue"), lty = c(1, 2), lwd = 2)
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7d73a5d45f89a28e922f48484b8c95df9f5333fd
